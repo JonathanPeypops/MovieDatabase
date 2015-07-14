65ff0596f075a33da8f6c72f66ebb3030370494a
Kevin Van Robbroeck <kevin.vanrobbroeck@gmail.com>
2015-07-14 15:07:41 +0200

    Removed table prefixes (causes portability issues with other users who have a different database schema)

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,6 @@
 INSERT INTO film(id, title, filmlength, director, genre) VALUES (1, 'Harry Potter', 98, 'Lars', 'SCIFI');
 INSERT INTO film(id, title, filmlength, director, genre) VALUES (2, 'Iron Man', 87, 'Tony Stark', 'ACTION');
 INSERT INTO film(id, title, filmlength, director, genre) VALUES (3, 'The Purge', 103, 'Stijn', 'HORROR');
-INSERT INTO films.actor (id, bio, firstname, gender, lastname) VALUES (1, 'He is just awesome', 'Tony', 'MALE', 'Stark');
-INSERT INTO films.actor (id, bio, firstname, gender, lastname) VALUES (2, 'nah', 'This', 'FEMALE', 'Girl');
-INSERT INTO films.actor (id, bio, firstname, gender, lastname) VALUES (3, 'You shall not pass', 'Gandalf', 'MALE', 'The Grey');
+INSERT INTO actor (id, bio, firstname, gender, lastname) VALUES (1, 'He is just awesome', 'Tony', 'MALE', 'Stark');
+INSERT INTO actor (id, bio, firstname, gender, lastname) VALUES (2, 'nah', 'This', 'FEMALE', 'Girl');
+INSERT INTO actor (id, bio, firstname, gender, lastname) VALUES (3, 'You shall not pass', 'Gandalf', 'MALE', 'The Grey');
