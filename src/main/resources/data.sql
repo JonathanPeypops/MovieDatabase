5d42ac54815ee3e5366e9066903202480e36e73e
Jonathan Peypops <jonathanpeypops@msn.com>
2015-07-16 13:49:53 +0200

    update data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -6,7 +6,7 @@ INSERT INTO film_character (id, name, film_id, actor_id) VALUES (1, 'Harry Potte
 INSERT INTO film_character (id, name, film_id, actor_id) VALUES (2, 'Ron Weasley', 1, 2);
 INSERT INTO film_character (id, name, film_id, actor_id) VALUES (3, 'Hermione Granger', 1, 3);
 
-INSERT INTO film(id, title, filmlength, director, genre, trailer, coverimg) VALUES (2, 'Iron Man', 126, 'Jon Favreau', 'ACTION', 'https://www.youtube.com/embed/8hYlB38asDY', 'http://ia.media-imdb.com/images/M/MV5BMTczNTI2ODUwOF5BMl5BanBnXkFtZTcwMTU0NTIzMw@@._V1_SX214_AL_.jpg');
+INSERT INTO film(id, title, filmlength, director, genre, trailer, coverimg) VALUES (2, 'Iron Man', 126, 'Jon Favreau', 'ACTION', 'https://www.youtube.com/embed/8hYlB38asDY', 'https://orig10.deviantart.net/ff09/f/2012/134/3/e/the_avengers___ironman_by_stephencanlas-d4zpaxl.jpg');
 INSERT INTO actor (id, bio, firstname, gender, lastname, actorimg) VALUES (4, 'He is Tony Stark', 'Robert', 'MALE', 'Downey Jr.', 'http://ia.media-imdb.com/images/M/MV5BMTAwNjk2NTUyMzleQTJeQWpwZ15BbWU3MDQ2NzQzMTc@._V1_UY317_CR2,0,214,317_AL_.jpg');
 INSERT INTO actor (id, bio, firstname, gender, lastname, actorimg) VALUES (5, 'He is Jarvis', 'Paul', 'MALE', 'Bettany', 'http://ia.media-imdb.com/images/M/MV5BMjEwODg1MTA5Ml5BMl5BanBnXkFtZTcwNDQwMTQxMw@@._V1_UY317_CR11,0,214,317_AL_.jpg');
 INSERT INTO actor (id, bio, firstname, gender, lastname, actorimg) VALUES (6, 'She is Pepper Potts', 'Gwyneth', 'FEMALE', 'Paltrow', 'http://ia.media-imdb.com/images/M/MV5BNzIxOTQ1NTU1OV5BMl5BanBnXkFtZTcwMTQ4MDY0Nw@@._V1_UX214_CR0,0,214,317_AL_.jpg');
@@ -14,7 +14,7 @@ INSERT INTO film_character (id, name, film_id, actor_id) VALUES (4, 'Iron Man', 
 INSERT INTO film_character (id, name, film_id, actor_id) VALUES (5, 'Pepper Potts', 2, 5);
 INSERT INTO film_character (id, name, film_id, actor_id) VALUES (6, 'Pepper', 2, 6);
 
-INSERT INTO film(id, title, filmlength, director, genre, trailer, coverimg) VALUES (3, 'The Purge', 85, 'James DeMonaco', 'HORROR', 'https://www.youtube.com/embed/K0LLaybEuzA', 'http://cdn.bloody-disgusting.com/wp-content/uploads/2013/04/Purge_Quad_4_3_13.jpg');
+INSERT INTO film(id, title, filmlength, director, genre, trailer, coverimg) VALUES (3, 'The Purge', 85, 'James DeMonaco', 'HORROR', 'https://www.youtube.com/embed/K0LLaybEuzA', 'http://starseeker.com/wp-content/uploads/2013/04/the-purge.jpg');
 INSERT INTO actor (id, bio, firstname, gender, lastname, actorimg) VALUES (7, 'He is James Sandin', 'Ethan', 'MALE', 'Hawke', 'http://ia.media-imdb.com/images/M/MV5BMTk4NDMxMTI0MF5BMl5BanBnXkFtZTYwMjE3ODE0._V1_UY317_CR5,0,214,317_AL_.jpg');
 INSERT INTO actor (id, bio, firstname, gender, lastname, actorimg) VALUES (8, 'He is Charlie Sandin', 'Max', 'MALE', 'Burkholder', 'http://ia.media-imdb.com/images/M/MV5BMTk3MzE2ODA5Ml5BMl5BanBnXkFtZTgwOTUxNTE2NTE@._V1_UX214_CR0,0,214,317_AL_.jpg');
 INSERT INTO actor (id, bio, firstname, gender, lastname, actorimg) VALUES (9, 'She is Mary Sandin', 'Lena', 'FEMALE', 'Headey', 'http://ia.media-imdb.com/images/M/MV5BMzIwMjIwNjg0M15BMl5BanBnXkFtZTgwOTI3MDEzMDE@._V1_UY317_CR14,0,214,317_AL_.jpg');
